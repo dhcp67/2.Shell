82c0707ce7a2d71281828bd02a3131d9667d7470
lcnihao <318082789@qq.com>
2019-05-24 18:11:52 +0800

    update

--- Prime.sh
+++ Prime.sh
@@ -3,9 +3,15 @@ for (( i = 1; i <= 1000; i++)); do
 	arr[$i]=0
 done
 for (( i = 2; i*i <= 1000; i++)); do
+    if [[ $arr[$i] -ne 0 ]]; then
+        continue
+    fi
 	for(( j = i; i * j <= 1000; j++)); do
 		arr[$[ i*j ]]=1
 	done
+done
+for i in `seq 1 10`; do
+        echo ${arr[$i]}
 done
 sum=0
 ret=1
