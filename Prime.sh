1cb052d2abbf2804419d5f08761c0e9687535d30
lcnihao <318082789@qq.com>
2019-05-24 18:18:10 +0800

    update

--- Prime.sh
+++ Prime.sh
@@ -3,22 +3,19 @@ for (( i = 1; i <= 1000; i++)); do
 	arr[$i]=0
 done
 for (( i = 2; i*i <= 1000; i++)); do
-    if [[ $arr[$i] -ne 0 ]]; then
+    if [[ ${arr[$i]} -ne 1 ]]; then
         continue
     fi
 	for(( j = i; i * j <= 1000; j++)); do
 		arr[$[ i*j ]]=1
 	done
 done
-for i in `seq 1 10`; do
-        echo ${arr[$i]}
-done
 sum=0
 ret=1
 for (( i = 2; i <= 1000; i++)); do
 	if [[ ${arr[$i]} -ne 1 ]]; then
 		sum=$[ sum+i ]
-		arr[$ret]=arr[$i]
+		arr[$ret]=$i
         ret=$[ ret+1 ]
 	fi
 done
