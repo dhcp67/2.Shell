882be78efc7280e8cc32bc8efc47f58fd87cd3fb
lcnihao <318082789@qq.com>
2019-05-29 20:50:54 +0800

    rm

--- rm.sh
+++ rm.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-Hostname=`Hostname`
+Hostname=`hostname`
 echo "${Hostname}"
-
+Names=`cat /etc/passwd | awk -F: '{if ($3>1000 && $3 != 65534)  printf("%s ", $1)}'`
+for i in $Names; do
+    echo $i
+    File="/home/${i}/.ssh/authorized_keys"
+    echo $File
+    if [[ -f $File ]]; then 
+        #rm -f ${File} && 
+        echo "Rm OK !"
+        else
+            continue
+        fi
+done
