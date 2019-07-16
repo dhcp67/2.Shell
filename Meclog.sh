8a06a4869d27f722d3b5552705614c7bf53deef0
lcnihao <318082789@qq.com>
2019-07-16 20:45:13 +0800

    updata

--- Meclog.sh
+++ Meclog.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-NowTime=`date +"%Y-%m-%d-%H-%M-%S"`
-
 echo $NowTime
 
 DyAver=$1
@@ -10,4 +8,12 @@ if [[ ${DyAver}x == "x" ]];then
     exit 1
 fi
 
+MemValue=(`free -m | head -n 2| tail -n 1 | awk '{printf("%d %d", $3, $2)}'`)
 
+MemAvaPer=`echo "scale=1;${MemValue[0]}*100/$MemAvaPer[1]}" | bc`
+
+DyAver=`echo "scale=1;${DyAver}*0.3+${MemAvaPer}*0.7" | bc`
+
+NowTime=`date +"%Y-%m-%d-%H:%M:%S"`
+
+echo "${NowTime} ${MemValue[1]} ${MemAvaPer} "
